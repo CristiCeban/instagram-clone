7fbe6e58716c52a59b8b52c9f32ad420401d1d52
Alex <alexwarofcommand@gmail.com>
2020-12-05 12:52:52 +0200

    added jwt utilities

--- server/src/main/java/com/buy_posts/BuyPostsApplication.java
+++ server/src/main/java/com/buy_posts/BuyPostsApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication()
+
+@SpringBootApplication(exclude=SecurityAutoConfiguration.class)
 public class BuyPostsApplication {
     public static void main(String[] args) {
         SpringApplication.run(BuyPostsApplication.class, args);
---
+++ server/src/main/java/com/buy_posts/Model/JwtResponse.java
@@ -0,0 +1,21 @@
+package com.buy_posts.Model;
+
+import java.io.Serializable;
+
+public class JwtResponse implements Serializable{
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    
+	private final String jwttoken;
+
+	public JwtResponse(String jwttoken) {
+		this.jwttoken = jwttoken;
+	}
+
+	public String getToken() {
+		return this.jwttoken;
+	}
+}
