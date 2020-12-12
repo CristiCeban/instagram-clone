b2b88c50a92822ee910ef713b45cf1bfff40cffe
CristiCeban <cebancristi4444@gmail.com>
2020-12-12 18:23:56 +0200

    to resolve cors

--- client/src/redux/actions/productsActions.ts
+++ client/src/redux/actions/productsActions.ts
@@ -1,4 +1,6 @@
 import {Dispatch} from "react";
+import ApiService from '../../services/api'
+
 
 export interface UploadProduct {
     readonly type : 'UPLOAD_PRODUCT'
@@ -18,6 +20,8 @@ export const onUploadProduct = (values : any) => {
     return async(dispatch : Dispatch<ProductsActions>) => {
         try {
             dispatch({type:'SET_IN_PROGRESS_PRODUCT',payload:true})
+            console.log(values)
+            const response = ApiService.post('products/add',values)
             console.log(values)
         }
         catch (e) {
