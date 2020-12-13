8187ceb3854132d909bc40cc2b18615165183c4a
CristiCeban <cebancristi4444@gmail.com>
2020-12-13 14:37:42 +0200

    doing profile screen

--- client/src/functions/createFormData.ts
+++ client/src/functions/createFormData.ts
@@ -1,13 +1,13 @@
 import {UploadProductType} from "../redux/types/productTypes";
 
 
-export const createUploadProductFormData = ({name,price,short_description,longDescription,category_id,files} : UploadProductType) => {
+export const createUploadProductFormData = ({name,price,short_description,longDescription,categoryId,files} : UploadProductType) => {
     const formData = new FormData();
     formData.append('name',name)
     formData.append('price',price.toString())
     formData.append('short_description',short_description);
     formData.append('longDescription',longDescription);
-    formData.append('category_id',category_id.toString());
+    formData.append('categoryId',categoryId.toString());
     files.map(file => formData.append('files',file))
     return formData;
 }
