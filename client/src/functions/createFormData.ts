88101d9ec12e401bfce828516a1ac711525bcf81
CristiCeban <cebancristi4444@gmail.com>
2020-12-13 15:27:34 +0200

    some refactor 2x

--- client/src/functions/createFormData.ts
+++ client/src/functions/createFormData.ts
@@ -1,12 +1,12 @@
 import {UploadProductType} from "../redux/types/productTypes";
 
 
-export const createUploadProductFormData = ({name,price,short_description,longDescription,categoryId,files} : UploadProductType) => {
+export const createUploadProductFormData = ({name,price,shortDescription,longDescription,categoryId,files} : UploadProductType) => {
     const formData = new FormData();
     formData.append('name',name)
     formData.append('price',price.toString())
-    formData.append('short_description',short_description);
-    formData.append('longDescription',longDescription);
+    formData.append('short_description',shortDescription);
+    formData.append('long_description',longDescription);
     formData.append('categoryId',categoryId.toString());
     files.map(file => formData.append('files',file))
     return formData;
