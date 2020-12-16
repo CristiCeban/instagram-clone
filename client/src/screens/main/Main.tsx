05ffb2a6446ba6522b6b64ee9ba0636a24e7974e
CristiCeban <cebancristi4444@gmail.com>
2020-12-16 21:47:30 +0200

    edit profile done,to finish profile screen

--- client/src/screens/main/Main.tsx
+++ client/src/screens/main/Main.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Post from "../../components/post/Post";
+import {onGetProductsMain} from "../../redux/actions/productsActions";
+import {useDispatch} from "react-redux";
 
 
 const Main = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(onGetProductsMain())
+    },[])
         return (
                 <div style={{marginTop:100}}>
                     <Post/>
